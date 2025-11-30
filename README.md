448b5327a2b647d7ae1f2252611f10709020cf29
tr-Kalyan <kalyansde1@gmail.com>
2025-11-30 18:44:49 +0530

    Folder structure and openzeppelin installation

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-Our stable
+1. Relative Stability: Anchored or Pegged -> $1.00
+    1. Chainlink Price feed.
+    2. Set a function to exchange ETH & BTC -> $$$
+2. Stability Mechanism (Minting): Algorithmic (Decentralized)
+    1. People can only mint the stable coin with enough collateral (coded)
+3. Collateral: Exagenous (Crypto)
+    1. wETH
+    2. wBTC
---
+++ src/DecentralizedStableCoin.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+
+// This is considered an Exogenous, Decentralized, Anchored (pegged), Crypto Collateralized low volitility coin
+
+// Layout of Contract:
+// version
+// imports
+// interfaces, libraries, contracts
+// errors
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+pragma solidity ^0.8.18;
+
+import
+
+/**
+ * @title DecentralizedStableCoin
+ * @author Kalyan
+ * Collateral: Exogenous (ETH & BTC)
+ * Minting: Algorithmic
+ * Relative Stability: Pegged to USD
+ * 
+ * This is the contract meant to be governed by DSCEngine. This contract is just the ERC20 implementaion of our stablecoin system.
+ * 
+*/ 
+
+contract DecentralizedStableCoin {
+    constructor() {}
+}
