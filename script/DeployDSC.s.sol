46acf73ec57e1a5111c5708057951bde288e9a49
tr-Kalyan <kalyansde1@gmail.com>
2025-12-02 23:07:51 +0530

    Deploy and Helper Script

--- script/DeployDSC.s.sol
+++ script/DeployDSC.s.sol
@@ -1,1 +1,29 @@
 //SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+import { Script } from "forge-std/Script.sol";
+import {DecentralizedStableCoin} from "../src/DecentralizedStableCoin.sol";
+import {DSCEngine} from "../src/DSCEngine.sol";
+import {HelperConfig} from "./HelperConfig.sol";
+
+contract DeloyDSC is Script {
+
+    address[] public tokenAddresses;
+    address[] public priceFeedAddresses;
+
+    function run() external (DecentralizedStableCoin,DSCEngine) {
+        HelperConfig config = new HelperConfig();
+
+        (address wethUsdPriceFeed, address wbtcUsdPriceFeed, address weth, address wbtc, uint256 deployerKey) = config.activeNetworkConfig();
+
+        tokenAddresses = [weth,wbtc];
+        priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
+
+        vm.startBroadcast();
+        DecentralizedStableCoin dsc = new DecentralizedStableCoin();
+        DSCEngine engine = new DSCEngine(tokenAddresses, priceFeedAddresses,address(dsc));
+        dsc.transferOwnership(address(engine));
+        vm.stopBroadcast();
+        return (dsc, engine);
+    }   
+}
